9974f5a172779b0868ef5aec6cc6c6530ea17f7e
vikash kushwaha <vikashkush29@gmail.com>
2025-04-24 01:56:33 +0530

    feat: implement user and captain signup forms with state management and validation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {BrowserRouter} from 'react-router-dom'
 import Home from './pages/Home'
 import UserLogin from './pages/UserLogin'
-import UserSignup from './pages/UserSignup'
+import UserSignup from './pages/UserSignUp'
 import CaptainLogin from './pages/CaptainLogin'
 import CaptianSignup from './pages/CaptainSignup'
 import {Routes,Route} from 'react-router-dom'
+import UserContext, { UserDataContext } from './context/UserContext'
 const App = () => {
+
   return (
     <div>
 <Routes>
-   <Route path='/' element={<Home/>}/>
+   <Route path='/' element={<Home/>}/>          
    <Route path='/login' element={<UserLogin/>}/>
    <Route path='/signup' element={<UserSignup/>}/>
    <Route path='/captain-login' element={<CaptainLogin/>}/>
--- frontend/src/pages/CaptainSignup.jsx
+++ frontend/src/pages/CaptainSignup.jsx
@@ -1,8 +1,98 @@
-import React from 'react'
+import React, { use, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const CaptainSignup = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [firstName, setFirstname] = useState('')
+  const [lastName, setLastname] = useState('')
+  const [userdata, setuserData] = useState({})
+  
+  
+   const submitHandler=(e)=>{
+     e.preventDefault()
+     setuserData({
+      fullName:{
+        firstName:firstname,
+        lastName:lastname,
+      },
+      email:email,
+      password:password
+     })
+     console.log(userdata)
+     setEmail('')
+     setFirstname('')
+     setLastname('')
+     setPassword('')
+   }
   return (
-    <div>CaptainSignup</div>
+    <div className=' h-screen p-7 flex flex-col justify-between'>
+    <div>
+    <img className='w-16 mb-10 'src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt=" Uber photo black wala " />
+       <form  onSubmit={(e)=>{
+         submitHandler(e)
+       }} action="">
+        
+        <h3 className=' text-lg font-medium mb-2'>What's your name</h3>
+        <div className='flex  gap-4 mb-5'>
+        <input  
+         required
+         className='bg-[#eeeee]  rounded px-4 py-2 w-1/2 border text-base placeholder:text-sm'
+         type="text" 
+         placeholder='first name'
+         value={firstname}
+         onChange={(e)=>{
+          setFirstname(e.target.value)
+         }}
+         />
+
+<input  
+         required
+         className='bg-[#eeeee]  rounded px-4 py-2 border w-1/2  text-lg placeholder:text-base'
+         type="text" 
+         placeholder='last name' 
+         value={lastname}
+         onChange={(e)=>{
+          setLastname(e.target.value)
+         }}
+         />
+    </div>
+
+    
+         <h3 className=' text-lg font-medium mb-2'>What's your email</h3>
+         <input  
+         required
+         className='bg-[#eeeee] mb-6 rounded px-4 py-2 border w-full text-lg placeholder:text-base'
+         type="email" 
+         placeholder='email@gmail.com'
+         value={email}
+         onChange={(e)=>{
+          setEmail(e.target.value)
+         }}
+         />
+ 
+         <h3 className='text-lg font-medium mb-2 '>Enter Password</h3>
+         <input 
+         required 
+         className='bg-[#eeeee] mb-6 rounded px-4 py-2 border w-full text-lg placeholder:text-base'
+         type="password" 
+         placeholder='Password' 
+         value={password}
+         onChange={(e)=>{
+          setPassword(e.target.value)
+         }}
+         />
+ 
+         <button  className='bg-[#111] text-white font-semibold mb-2 rounded px-4 py-2  w-full text-lg placeholder:text-base'>login</button>
+ 
+         <p className='text-center'>Already have account? <Link  to='/captain-login'className='text-blue-600'>Login here</Link></p>
+       </form>
+    </div>
+   <p className='text-[10px] leading-tight'>This site is protected by reCAPTCHA and the 
+    <span className='underline'>Google policy</span> and <span className='underline'> Terms of Service apply </span>.
+       Privacy .</p>
+ 
+     </div>
   )
 }
 
